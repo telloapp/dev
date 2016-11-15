c582cc0884af3a05c3e43f873441720d365a54ec
lucky <lucky@tello.co.za>
2016-11-15 09:54:01 +0200

    initial commit new chat forum

---
+++ chat forum/pages/home.php
@@ -0,0 +1,51 @@
+<?php
+require '../core/init.php';
+
+$general->logged_out_protect();
+
+$designer_id = htmlentities($user['id']);
+
+
+//$m_id  = htmlentities($_POST['m_id']);
+//gg
+
+$view_notifications =  $chartforum->get_notifications($designer_id);
+
+$view_comments = $chartforum->get_msg_id($designer_id);
+
+
+//$chartforum->delete_notifications($m_id,$designer_id); // funtion to update status column to "read"
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Designer Dashboard</title>
+</head>
+<body>
+
+<form method="post" action="">
+	<h1>Welcome to Tello App</h1>
+	<p>What do you want to do ?</p>
+	<ol>
+		<li><a href="list_messages.php">view messages</a>
+
+	<?php foreach ($view_comments as $row) { ?>
+		<a href="list_myMsgs.php?designer_id=<?php echo $row['designer_id']?>">Notifications</a>
+	    <?php echo $num_rows = count($view_notifications); ?>
+	<?php } ?>
+		</li>
+	    <li><a href="send_message.php">Add Message</a></li>
+	    <li><a href="logout.php">logout</a></li>
+
+
+            
+	</ol>
+</form>
+
+<h1>Dashboard</h1>
+
+<br>
+</body>
+</html>
